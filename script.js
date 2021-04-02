93e797fefce68a0a4f191c9fcb14ec0e278996af
YounesMajeed <younesmajeed@gmail.com>
2021-04-02 16:50:30 +0530

    skelton and backend etc

---
+++ script.js
@@ -0,0 +1,28 @@
+const para = document.querySelector('p');
+const btn = document.querySelector('.getButton');
+const by = document.querySelector('.by');
+        
+btn.addEventListener('click', randomQuote);
+const dataURL = "https://api.quotable.io/random"
+// using fetch function
+   /* function randomQuote(){
+        //fetch function passed dataURL from above
+        fetch(dataURL)
+        .then(function(response){
+            return response.json();
+        }).then(function(data){
+            para.textContent = (data.content);
+            by.textContent = ('- ' + data.author)
+        })
+    }
+*/
+// using async and await
+    async function randomQuote() {
+    const response = await fetch(dataURL)
+    const data = await response.json()
+    //log just to check
+    console.log(`${data.content} —${data.author}`)
+    // showing content to the DOM
+    para.textContent = (data.content);
+    by.textContent = ('- ' + data.author);
+  }
